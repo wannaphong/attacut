9f2693a4ec3efea1d68e428ef0889ded4b0e6e8d
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2020-10-26 11:07:12 +0700

    Update __init__.py

--- attacut/artifacts/__init__.py
+++ attacut/artifacts/__init__.py
@@ -5,10 +5,11 @@ from attacut import logger
 log = logger.get_logger(__name__)
 
 artifact_dir = os.path.dirname(__file__)
+model_list = [name for name in os.listdir(artifact_dir) if os.path.isdir(os.path.join(artifact_dir,name))]
 
 
 def get_path(name: str) -> str:
-    if name in ["attacut-c", "attacut-sc"]:
+    if name in model_list:
         return f"{artifact_dir}/{name}"
     else:
         # if name isn't in the list, then it's a custom model
